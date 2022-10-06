f7225d07d167053098300205184650d378c56c0d
Aminesouhail <souhailamine97@gmail.com>
2022-10-06 20:45:57 +0100

    tt

---
+++ main.js
@@ -0,0 +1,37 @@
+
+ let title = document.getElementById('title');
+ let price = document.getElementById('price');
+ let taxes = document.getElementById('taxes').ariaPlaceholder;
+ let ads = document.getElementById('ads');
+ let discount = document.getElementById('discount');
+ let total =document.getElementById('total');
+ let count = document.getElementById('count');
+ let category = document.getElementById('category');
+ let submit = document.getElementById('submit');
+//  console.log(title,price,taxes,ads,ads,total,count,category,submit);
+                   //    map    //
+//get total 
+console.log(taxes)
+    function getTot() { 
+        console.log ('hello')
+    
+};
+
+
+
+
+
+
+
+
+//create product 
+// save localstorage 
+// clear inputs 
+// read 
+// count 
+// delete 
+// update 
+//search 
+// clean data 
+    
+ 
